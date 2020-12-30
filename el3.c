75dec92db30ca62ee2767cad380baae51ad41afe
Simon Willcocks <simon.willcocks@gmx.de>
2020-12-30 16:30:49 +0100

    Intermediate step without any support for threads or message passing
    Simply provide a routine you'd like called at secure el1 for each core.
    Modify include/kernel.h for core-specific storage at the beginning of the Core structure, leaving space for a stack at the top of the structure.
    EL3 becomes irrelevant.
    
    No exciting MMU action; see the IsambardOS branch for examples of that.

--- el3.c
+++ el3.c
@@ -18,6 +18,33 @@ static inline int read_number_of_cores()
   return ((res >> 24) & 0x3) +1;
 }
 
+
+typedef void __attribute__(( noreturn )) (*secure_el1_code)( Core *core, int number, uint64_t volatile *present );
+
+static void __attribute__(( noreturn )) run_at_secure_el1( Core *core, int number, uint64_t volatile *present, secure_el1_code code )
+{
+  asm volatile ( "\tmsr sp_el1, %[SP]"
+               "\n\tmov sp, %[SP]"
+               "\n\tmsr spsr_el3, %[PSR]"
+               "\n\tmsr elr_el3, %[EL1]"
+               "\n\tmov x0, %[CORE]"
+               "\n\tmov x1, %[NUMBER]"
+               "\n\tmov x2, %[PRESENT]"
+               "\n\teret"
+               :
+               : [SP] "r" (core+1)
+               , [EL1] "r" (code)
+               , [CORE] "r" (core)
+               , [NUMBER] "r" (number)
+               , [PRESENT] "r" (present)
+               , [PSR] "r" (0xc5) // EL1, using SP_EL1, interrupts disabled
+               : "x0", "x1", "x2" );
+
+  __builtin_unreachable();
+}
+
+extern void __attribute__(( noreturn )) enter_secure_el1( Core *phys_core, int number, uint64_t volatile *present );
+
 void __attribute__(( noreturn, noinline )) c_el3_nommu( Core *core, int number )
 {
   if (number == 0) {
@@ -34,7 +61,45 @@ void __attribute__(( noreturn, noinline )) c_el3_nommu( Core *core, int number )
   while (first_free_page == 0 || number_of_cores == 0) {}
   // The variables have been initialised by core 0
 
-  for (;;) { } // This is where you do stuff, without returning
+  static uint64_t volatile resume = 0;
+  static uint64_t volatile present = 0;
+
+  if (number == 0) {
+    resume = (1ull << number_of_cores) - 1;
+    // In subsequent code segments, use resume = present;
+  }
+  else {
+    // Wait for resume to be initialised
+    while (resume == 0) {}
+  }
+
+  switch (number) {
+#ifdef EL3_RAW_DISPLAY
+  case EL3_RAW_DISPLAY:
+    {
+extern void __attribute__(( noreturn )) el3_no_mmu_display_pages( Core *core );
+
+      clearbit( &resume, number );
+      el3_no_mmu_display_pages( core );
+      // (This core will not be "present", for the rest of the routine.)
+    }
+    break;
+#endif
+  default:
+    {
+      // Insert code that will not block forever, here
+      setbit( &present, number );
+      clearbit( &resume, number );
+    }
+    break;
+  }
+
+  while (resume != 0) {}
+  // All the cores have executed the preceding switch, by this point,
+  // or are never going to return from it. The ones that did return
+  // have set a bit in present, which can be used to initialise resume
+  // for the next sync.
+  run_at_secure_el1( core, number, &present, enter_secure_el1 );
 
   __builtin_unreachable();
 }
