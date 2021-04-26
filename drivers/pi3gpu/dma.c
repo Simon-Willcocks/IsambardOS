f75a9e98472bec19ec8805689f4617bb3073ffaa
Simon Willcocks <simon.willcocks@gmx.de>
2021-04-26 17:45:29 +0200

    Virtual machine implementation for arm32 code, with IRQ enter/exit
    Next step is to trigger events to return to secure mode, handle them
    and continue the guest OS and applications.

---
+++ drivers/pi3gpu/dma.c
@@ -0,0 +1,49 @@
+/* Copyright (c) 2021 Simon Willcocks */
+
+#include "devices.h"
+
+ISAMBARD_INTERFACE( TRIVIAL_NUMERIC_DISPLAY )
+
+#include "interfaces/client/TRIVIAL_NUMERIC_DISPLAY.h"
+
+extern TRIVIAL_NUMERIC_DISPLAY tnd;
+
+#define N( n ) NUMBER__from_integer_register( n )
+
+void *memset(void *s, int c, unsigned long long n)
+{
+  char *p = s;
+  for (unsigned long long i = 0; i < n; i++) {
+    p[i] = c;
+  }
+  return s;
+}
+
+void expose_dma() {}
+/*
+{
+  if (!current_device_bits_match()) return; // Should be calculated at compile time
+
+  tnd = TRIVIAL_NUMERIC_DISPLAY__get_service( "Trivial Numeric Display", -1 );
+  debug_progress = 0x11;
+  //start_show_page_thread();
+  debug_progress = 0x33;
+
+  TRIVIAL_NUMERIC_DISPLAY__show_32bits( tnd, N( 900 ), N( 10 ), N( this_thread ), N( 0xfff0f0f0 ) );
+TRIVIAL_NUMERIC_DISPLAY__show_32bits( tnd, N( 1700 ), N( 20 ), N( mapped_memory_pa ), N( 0xffffffff ) ); asm( "svc 0" );
+  debug_progress = 1;
+
+  for (int i = 0; i < 100; i++) { sleep_ms( 20 ); mapped_memory[2] = i; }
+
+TRIVIAL_NUMERIC_DISPLAY__show_32bits( tnd, N( 1600 ), N( 20 ), N( mapped_memory_pa ), N( 0xffffffff ) ); asm( "svc 0" );
+  debug_progress = 3;
+  if (!prepare_for_interrupts_from_sd_port()) return;
+
+  debug_progress = 19;
+  if (!initialise_sd_interface()) return;
+  debug_progress = 20;
+
+  EMMC__BLOCK_DEVICE__register_service( "DMA_MANAGER", &emmc_service_singleton );
+}
+
+*/
---
+++ include/kernel_translation_tables.h
@@ -0,0 +1,61 @@
+
+void initialise_shared_isambard_kernel_tables( Core *core0, int cores );
+void set_asid( Core *core, uint64_t asid );
+
+// Registers set at EL1, secure and non-secure
+// Order is fixed for el3_virtual_machines.c assembler
+typedef struct {
+  uint64_t cntkctl_el1;
+  uint64_t csselr_el1;
+  uint64_t mair_el1;
+  uint64_t sctlr_el1;
+  uint64_t tcr_el1;
+  uint64_t ttbr0_el1; // Core-specific, in secure mode
+  uint64_t ttbr1_el1;
+  uint64_t vbar_el1;
+  // uint64_t spsr_el1; Thread-specific
+  // uint64_t elr_el1; Thread-specific
+  // TPIDR_EL1
+
+  uint64_t vttbr_el2;
+  uint64_t hcr_el2;
+  uint64_t hstr_el2;
+  uint64_t vmpidr_el2;
+  uint64_t vpidr_el2;
+  uint64_t vtcr_el2;
+
+  // contextidr_el2, CPTR_EL2, DACR32_EL2, HACR_EL2, RMR_EL2, RMR_EL2, TPIDR_EL2; No use for these registers
+  // ESR_EL2, FAR_EL2, HPFAR_EL2, IFSR32_EL2; passed to partner thread to inform of exceptions
+  // sctlr_el2, tcr_el2, mair_el2, vbar_el2; Relates to Isambard VM implementation, doesn't change
+} vm_state;
+
+extern vm_state vm[2]; // Entry 0 is for secure mode, all others for non-secure
+
+static const uint64_t himem_offset = 0xfffffffffe000000;
+
+#ifndef EL_PARAMETERS
+#define EL_PARAMETERS Core *core, int number, uint64_t *present
+#define EL_ARGUMENTS core, number, present
+#endif
+
+typedef void __attribute__(( noreturn )) (*isambard_init_code)( EL_PARAMETERS );
+typedef void __attribute__(( noreturn )) (*isambard_code_caller)( EL_PARAMETERS, isambard_init_code code );
+
+typedef enum { WAITING, NORMAL, SPECIAL } core_types;
+
+// Routine provided by chosen el3_ behaviour source file, called once, during
+// initialisation.
+// The routine may set present[number] = NORMAL and return, or set it to
+// SPECIAL, not return, and run independently of the kernel.
+extern void roll_call( core_types *present, unsigned number );
+
+// Routine provided by chosen el3_ behaviour source file, called once, during
+// initialisation, finally entering `code', at Secure EL1 with no MMU.
+extern void __attribute__(( noreturn )) el3_synchronised_initialise( EL_PARAMETERS );
+
+// Entry routine for IsambardOS at EL1
+extern void __attribute__(( noreturn )) isambard_secure_el1( Core *phys_core, int number, uint64_t *present );
+
+extern void __attribute__(( noreturn )) el3_run_at_secure_el1( EL_PARAMETERS, isambard_init_code code );
+
+extern void __attribute__(( noreturn )) el3_run_with_mmu( EL_PARAMETERS, isambard_init_code code );
